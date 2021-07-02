be63c8cf2c0f270f44e02016ffc3b5534c2ebcd2
MikeyCouture <michelcouture42@gmail.com>
2021-07-02 16:39:47 -0400

    changing methods

--- script.js
+++ script.js
@@ -69,9 +69,19 @@ const renderCountry = function(data, className = '') {
 // };
 
 const getCountryData = function(country) {
+  //country 1
   fetch(`https://restcountries.eu/rest/v2/name/${country}`)
     .then(response => response.json())
-    .then(data => renderCountry(data[0]));
+    .then(data => {
+      renderCountry(data[0]);
+      const neighbour = data[0].borders[0];
+
+      if (!neighbour) return;
+      //country 2
+      return fetch(`https://restcountries.eu/rest/v2/alpha/${neighbour}`);
+    })
+    .then(response2 => response2.json())
+    .then(data => renderCountry(data, 'neighbour'));
 };
 
 getCountryData('Canada');
